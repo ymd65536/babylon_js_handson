5673d4a4d08658df428d3b57ccd5c436fed57a96
ymd65536 <ymd65536@gmail.com>
2022-04-22 23:26:45 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var createScene = function () {
+  const scene = new BABYLON.Scene(engine);
+  // ↓ここでモデルのロード
+  BABYLON.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon");
+
+  const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+  camera.attachControl(canvas, true);
+  const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(5, 1, 0));
+
+  return scene;
+};
---
+++ js/template.js
@@ -0,0 +1,10 @@
+var createScene = function () {
+  var scene = new BABYLON.Scene(engine);
+
+  // シーンにカメラを追加し、それを canvas にアタッチ
+  // シーンにライト（光源）を追加
+
+  // あなたのコード
+
+  return scene;
+};
