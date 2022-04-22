874dcfebae02e9194c2acb2817b2203f9a0331a6
ymd65536 <ymd65536@gmail.com>
2022-04-23 01:10:56 +0900

    バウンスサウンド

--- js/index.js
+++ js/index.js
@@ -15,8 +15,8 @@ var createScene = function () {
 
   const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
 
-  const sound = new BABYLON.Sound("name", "url to sound file", scene, null, { loop: true, autoplay: true });
-  sound.play();
+  const bounce = new BABYLON.Sound("bounce", "sounds/bounce.wav", scene);
+  setInterval(() => bounce.play(), 3000);
 
   return scene;
 };
