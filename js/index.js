1a90edb4b307e94fc6d7b131c8c94c763aabf367
ymd65536 <ymd65536@gmail.com>
2022-04-24 01:22:46 +0900

    matrerial-for-each-house-side

--- js/index.js
+++ js/index.js
@@ -12,7 +12,18 @@ const createScene = function (canvas, engine) {
   const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
 
   // ここにモデルを定義する - box1 box2 box3 の Mesh
-  const box = BABYLON.MeshBuilder.CreateBox("box", {});
+  const faceUV = [];
+  faceUV[0] = new BABYLON.Vector4(0.6, 0.0, 1.0, 1.0); //rear face
+  faceUV[1] = new BABYLON.Vector4(0.0, 0.0, 0.4, 1.0); //front face
+  faceUV[2] = new BABYLON.Vector4(0.4, 0, 0.6, 1.0); //right side
+  faceUV[3] = new BABYLON.Vector4(0.4, 0, 0.6, 1.0); //left side
+
+  const box = BABYLON.MeshBuilder.CreateBox("box", {width: 2, faceUV: faceUV, wrap: true});
+
+  // 外壁をデザイン
+  const boxMat = new BABYLON.StandardMaterial("boxMat");
+  boxMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/semihouse.png")
+  box.material = boxMat;
   box.position.y = 0.5;
 
   // 家を建築する(box に屋根をつける)
@@ -20,21 +31,15 @@ const createScene = function (canvas, engine) {
     "roof",
     {diameter: 1.3, height: 1.2, tessellation: 3}
   );
-  roof.scaling.x = 0.75;
-  roof.rotation.z = Math.PI / 2;
-  roof.position.y = 1.22;
-  box.rotation.y = Math.PI / 4;
-  roof.rotation.y = Math.PI / 4;
 
   // 屋根に色をつける
   const roofMat = new BABYLON.StandardMaterial("roofMat");
   roofMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/roof.jpg");
   roof.material = roofMat;
-
-  // 外壁をデザイン
-  const boxMat = new BABYLON.StandardMaterial("boxMat");
-  boxMat.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png")
-  box.material = boxMat
+  roof.scaling.x = 0.75;
+  roof.scaling.y = 2;
+  roof.rotation.z = Math.PI / 2;
+  roof.position.y = 1.22;
 
   // box を置くための地面を作る
   const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 10, height: 10 });
